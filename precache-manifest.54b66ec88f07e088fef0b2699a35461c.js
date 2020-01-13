1286ace762f4e0ac36bc77293f4c4545d1eef86a
rajyash05 <rajyash05@gmail.com>
2020-01-13 18:18:13 +0530

    Updates

---
+++ precache-manifest.54b66ec88f07e088fef0b2699a35461c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188231a08c07013a0975d15bf9397f9d",
+    "url": "/blogSite/index.html"
+  },
+  {
+    "revision": "8a04c559ec7a866a13be",
+    "url": "/blogSite/static/css/main.48705940.chunk.css"
+  },
+  {
+    "revision": "69062b326c7a45ba9547",
+    "url": "/blogSite/static/js/2.2960ddf2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/blogSite/static/js/2.2960ddf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a04c559ec7a866a13be",
+    "url": "/blogSite/static/js/main.393c781e.chunk.js"
+  },
+  {
+    "revision": "5d18861d5589f4013365",
+    "url": "/blogSite/static/js/runtime-main.9e88b78e.js"
+  }
+]);
